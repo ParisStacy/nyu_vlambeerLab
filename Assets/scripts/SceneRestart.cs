b2203934766c9286d28f223c7f2530b7f2aee044
ParisStacy <parispluto@gmail.com>
2020-10-25 14:35:28 -0700

    Complete 2

---
+++ Assets/scripts/SceneRestart.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneRestart : MonoBehaviour
+{
+
+    [SerializeField]
+    Pathmaker pathmaker;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R)) {
+            Application.LoadLevel(Application.loadedLevel);
+            pathmaker.Reset();
+        }
+    }
+}
